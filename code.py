4f9ac347453922e14eaef09d41713d3973a133cb
vpriyathegreat <95736159+vpriyathegreat@users.noreply.github.com>
2024-01-12 04:46:37 +0530

    Update code.py

--- code.py
+++ code.py
@@ -73,9 +73,11 @@ if selected == "Prediction":
         st.write("Title:", title_input)
         st.write("Bodymark:", bodymark_input)
         st.write("Tags:", tags_input)
-        st.write("Naive Bayes Predicted OpenStatus:", predictedvaive)
-        st.write("MLP Classifier Predicted OpenStatus:", predictedmlp)
-        if predictedvaive != 1:  # If not predicted to be an open question
+       # st.write("Naive Bayes Predicted OpenStatus:", predictedvaive)
+        #print("Naive Bayes Predicted OpenStatus:", "Open" if predicted_nb == 1 else "Closed")
+        print("MLP Classifier Predicted OpenStatus:", "Open" if predicted_mlp == 1 else "Closed")
+        st.write(" Predicted OpenStatus:", predictedmlp)
+        if predicted_mlp != 1:  # If not predicted to be an open question
             open_question_tags = ["python", "data-analysis", "machine-learning", "programming", "help"]
             st.write("Suggestions for closed questions:", open_question_tags)
         # else:
