f968f44e1dc3117cda7ab7309e1a434063562671
vpriyathegreat <95736159+vpriyathegreat@users.noreply.github.com>
2024-01-12 20:02:11 +0530

    Update code.py

--- code.py
+++ code.py
@@ -33,7 +33,7 @@ with st.sidebar:
         default_index=0
     )
 #predict similiar questions
-url = "xtrain_tfidf.zip"  # Adjust the filename as needed
+url = "https://github.com/vpriyathegreat/questions/blob/main/xtrain_tfidf.zip" # Adjust the filename as needed
 response = requests.get(url)
 xtrain_tfidf = pd.read_pickle(BytesIO(response.content))
 #predict the status
